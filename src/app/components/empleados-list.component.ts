7ddc2cd8ce4f632aaa2dbcaf7a38224a841f42a3
Alejandro <alejandrolistc@hotmail.com>
2017-06-29 22:07:09 -0500

    se muestra el listado de los empleados

--- src/app/components/empleados-list.component.ts
+++ src/app/components/empleados-list.component.ts
@@ -1,22 +1,36 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { EmpleadoService } from '../services/empleado.service';
+import { Empleado } from '../models/empleado';
 
 @Component({
   selector: 'empleados-list',
-  templateUrl: '../views/empleados-list.html'
+  templateUrl: '../views/empleados-list.html',
+  providers: [EmpleadoService]
 })
 
 export class EmpleadosListComponent{
   public titulo:string;
+  public empleados: Empleado[];
 
   constructor(
     private _route: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _empleadoService: EmpleadoService
   ){
     this.titulo = 'Listado de empleados';
   }
 
   ngOnInit(){
     console.log("EmpleadosListComponent cargado");
+
+    this._empleadoService.getEmpleados().subscribe(
+      result => {
+          this.empleados = result;
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
   }
 }
---
+++ src/app/models/empleado.ts
@@ -0,0 +1,8 @@
+export class Empleado{
+  constructor(
+    public id: number,
+    public nombre: string,
+    public apellidos: string,
+    public fechaNacimiento: number
+  ){ }
+}
