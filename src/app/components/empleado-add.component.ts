a54b69f476abc99f32e98eea9577dee8a0fd4423
Alejandro <alejandrolistc@hotmail.com>
2017-07-01 23:10:20 -0500

    maquetacion formulario ver-editar-eliminar y metodos para agregar imagen

--- src/app/components/empleado-add.component.ts
+++ src/app/components/empleado-add.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { EmpleadoService } from '../services/empleado.service';
 import { Empleado } from '../models/empleado';
+import { GLOBAL } from '../services/global';
 
 @Component({
   selector: 'empleado-add',
@@ -11,6 +12,8 @@ import { Empleado } from '../models/empleado';
 export class EmpleadoAddComponent{
   public titulo: string;
   public empleado: Empleado;
+  public filesToUpload;
+  public resultUpload;
 
   constructor(
       private _empleadoService: EmpleadoService,
@@ -27,6 +30,23 @@ export class EmpleadoAddComponent{
 
   onSubmit(){
     console.log(this.empleado);
+  /*  Metodo para agregar imagen
+  if(this.filesToUpload.lenght >=1){
+    this._empleadoService.makeFileRequest(GLOBAL.url+'upload-file',[],this.filesToUpload).then((result)=>{
+      console.log(result);
+      this.resultUpload = result;
+      this.empleado.imagen = this.resultUpload.filename;
+      //se agrega el metodo para agregar todos los datos del empleado (si se incluye la imagen en el backend)
+    },(error)=>{
+      console.log(error);
+     });
+   }else{
+     this.saveEmpleado();
+   }
+    saveEmpleadro(){
+      this._empleadoService.addEmpleado ... bla bla bla (copiar lo de abajo xD)
+    }
+   */
 
     this._empleadoService.addEmpleado(this.empleado).subscribe(
       response => {
@@ -39,5 +59,12 @@ export class EmpleadoAddComponent{
           console.log('ERROR '+<any>error);
       }
       );
+  }
+
+/*Metodo para subir archivos de imagen*/
+
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+    console.log(this.filesToUpload);
   }
 }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -41,5 +41,31 @@
      console.error(error);
      return Observable.throw(error.json().error || 'server error');
    }
+
+   /*Metodo para subir imagenes*/
+   makeFileRequest(url: string, params: Array<string>, files: Array<File>){
+     return new Promise((resolve, reject )=>{
+       var formData: any = new FormData();
+       var xhr = new XMLHttpRequest();
+
+       for (var i =0; files.length ; i++){
+         formData.append('uploads[]', files[i], files[i].name);
+       }
+
+       xhr.onreadystatechange = function(){
+         if(xhr.readyState == 4){
+           if(xhr.status == 200){
+               resolve(JSON.parse(xhr.response));
+           }else{
+               reject(xhr.response);
+           }
+         }
+       };
+
+       xhr.open("POST", url, true);
+       xhr.send(formData);
+
+     });
+   }
  }
 
